f10eb959c89d26c92cc44cd30fcbdb6076ff6ae9
Tiesto Schouten <22230@ma-web.nl>
2018-11-15 09:17:01 +0100

    deleted imported demo for player model + start turret script

---
+++ Assets/Scripts/BasicTurret.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTurret : MonoBehaviour {
+
+    public Transform target;
+    public float range = 15;
+
+
+    public string enemyTag = "enemy";
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Updatetarget", 0f, 0.5f);
+	}
+	
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3
+        }
+
+
+    }
+
+    
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+   void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
